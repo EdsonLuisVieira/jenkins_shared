0ae23db5108bd07f7f7ce99e87eb57c975aaafd6
Edson Almeida <edson.almeida@dock.tech>
2020-07-08 23:35:26 -0300

    ajuste notify.groovy

--- src/libs/notify/slack.groovy
+++ src/libs/notify/slack.groovy
@@ -1,4 +1,6 @@
 #!groovy
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
 package libs.notify;
 
 def void notifyBuild(String buildStatus = 'STARTED', String channel = 'jenkins' , String newVersion = '0.0.0', String path = 'default') {
