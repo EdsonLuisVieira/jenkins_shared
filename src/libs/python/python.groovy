06a703d9074d2ae94816d8497fe5e4b5d8b5561f
EdsonLuisVieira <edson.l.v.almeida@outlook.com>
2020-07-09 12:06:17 -0300

    Update python.groovy

--- src/libs/python/python.groovy
+++ src/libs/python/python.groovy
@@ -3,7 +3,6 @@ package libs.python;
 
 def void build(String build_name) {
     script{
-        sh 'python3 -m pip install --target . -r requirements.txt'
-        sh "zip -r ${build_name}.zip ."
+        echo build_name
     } 
 }
