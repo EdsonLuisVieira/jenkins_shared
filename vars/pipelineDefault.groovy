b145eada408193899aa5b56dff3ba50eb9146f6e
EdsonLuisVieira <edson.l.v.almeida@outlook.com>
2020-07-09 11:48:34 -0300

    Update pipelineDefault.groovy

--- vars/pipelineDefault.groovy
+++ vars/pipelineDefault.groovy
@@ -21,7 +21,6 @@ def call(Map stageParams) {
         stage('echovars'){
             steps{
                 script{
-                    echo (stageParams.RUN_PRE_BUILD)
                     echo env.RUN_PRE_BUILD
                     echo "${RUN_PRE_BUILD}"
                     echo stageParams.RUN_POST_BUILD
