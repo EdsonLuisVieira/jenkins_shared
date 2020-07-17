5b54a07181062cc71f081d2c093d0bc46571866e
Edson Luis Vieira de Almeida <edson.l.v.almeida@outlook.com>
2020-07-17 14:59:43 -0300

    teste mvars2

--- vars/echomap.groovy
+++ vars/echomap.groovy
@@ -1,4 +1,19 @@
 #!groovy
 def call(String stageParams) {
     echo stageParams
+    
+pipeline {
+   agent any
+
+   stages {
+      stage('Hello') {
+         steps {
+            echo 'stageParams'
+         }
+      }
+   }
+}
+
+
+
 }
