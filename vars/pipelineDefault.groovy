e7a52e0a1b47c444affa5c58d0465c8736515917
EdsonLuisVieira <edson.l.v.almeida@outlook.com>
2020-08-16 16:16:51 -0300

    Update pipelineDefault.groovy

--- vars/pipelineDefault.groovy
+++ vars/pipelineDefault.groovy
@@ -6,7 +6,6 @@ def call(Map stageParams) {
     def git = new libs.git.git()
     def notify = new libs.notify.slack()
     def rundeck = new libs.deploy.rundeck()
-    def build = new libs.cf.cf()
     def python = new libs.python.python()
     def resource = libraryResource 'globalvars/vars.yaml'
     def resourceVars = readYaml text: resource
